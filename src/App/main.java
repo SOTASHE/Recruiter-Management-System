e37cc06e109160b40ed9290bd52f06b5925fc08b
regragui <regragui@DESKTOP-T9HK4V4>
2017-03-14 12:52:20 +0000

    commit to pull

--- src/App/main.java
+++ src/App/main.java
@@ -3,19 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import App.Orm.DaoCandidates;
 import App.Orm.DaoRecruiter;
 import static App.Services.LinkedinService.profileToJson;
 import App.Services.ChartService;
+import App.Services.LinkedinService;
 import App.Services.StatsService;
 import App.Services.loginAgencyService;
+import OrmMapping.Candidates;
 import Ui.Services.StarRater;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
@@ -26,16 +31,16 @@ import org.json.JSONObject;
 public class main {
    
     
-    public static void main(String[] args) {
-       /* JFrame frame = new JFrame();
+    public static void main(String[] args) throws JSONException, IOException, InterruptedException {
+       JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        StarRater starRater = new StarRater(5, 2, 1);       
+        StarRater starRater = new StarRater(5, 2);       
         panel.add(starRater);
         frame.getContentPane().add(panel);
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        /*   
+         
       List L = StatsService.getStatsPorcentage();
       
         ChartService.CamembertJob(L,"Job");
@@ -77,11 +82,13 @@ public class main {
         
        */
        
-      if(loginAgencyService.getConnected("ilias.naamane1@gmail.com", "123456")){
+     /* if(loginAgencyService.getConnected("ilias.naamane1@gmail.com", "123456")){
            System.out.println("je m'appelle ilias");
       }
       else{
           System.out.println("hahouwa");
-      }
-    }  
-}
+      }*/
+      
+        
+
+}}
