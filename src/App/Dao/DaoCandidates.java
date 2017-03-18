15f4f3533cd63a9b9613342b5a0b102c09ea5539
regragui <regragui@DESKTOP-T9HK4V4>
2017-03-18 03:06:23 +0000

    Merge origin/master
    Conflicts:
    	src/Ui/Frames/AfficheCandidat.form
    	src/Ui/Frames/AfficheCandidat.java

--- src/App/Dao/DaoCandidates.java
+++ src/App/Dao/DaoCandidates.java
@@ -35,8 +35,13 @@ public class DaoCandidates extends DaoAbstract{
                 " where CandidatesId = " + idcandidate + " and Jobsid = " + idjob );
     }
    
-    // add function to get all current candidat by job with details phase entretien etc..
     
+    public List getEmployeesByJob(int idJob){
+        return super.getHqlQuery("select c.id,c.name,c.email,cj.satisfaction from Candidates c join "
+                + "c.candidatesJobses cj "+ "where cj.satisfaction > 0 and cj.id.jobsid = "+idJob 
+        );
+       
+    }
    
     
     
