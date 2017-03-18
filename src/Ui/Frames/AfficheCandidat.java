61370cb4ef974e3a13ffa40685c1bc2da4449daa
regragui <regragui@DESKTOP-T9HK4V4>
2017-03-18 03:08:52 +0000

    radio Box done

--- src/Ui/Frames/AfficheCandidat.java
+++ src/Ui/Frames/AfficheCandidat.java
@@ -325,6 +325,9 @@ public class AfficheCandidat extends javax.swing.JPanel {
     }//GEN-LAST:event_chercherActionPerformed
 
     private void inviterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inviterActionPerformed
+        
+        
+        
         if(EnvoiMessge()){
             int[] Rows;
             Rows = tCandidat.getSelectedRows();
